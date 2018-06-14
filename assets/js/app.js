11f7e4aad5bd508991a2dd517087937692c3036f
Daniel Kagemann <info@danielkagemannn.name>
2018-06-14 06:49:19 +0200

    some additions

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,8 @@ function Profile() {
       prompt.setAttribute("class", "prompt");
 
       this.content.append(prompt);
+
+      this.content.scrollTo(0,this.content.scrollHeight);
    };
    /**
     * create new answer on container
@@ -17,11 +19,9 @@ function Profile() {
    this.newAnswer = function (text) {
       var prompt = document.createElement("div");
       prompt.setAttribute("class", "answer");
-
-      //var child = document.createTextNode(text);
-      //prompt.appendChild(child);
       prompt.innerHTML = text + "<br/><br/>";
       this.content.append(prompt);
+      this.content.scrollTo(0,this.content.scrollHeight);
    };
 
    /**
@@ -68,8 +68,8 @@ function Profile() {
       var self = this;
       Typer('#q_skill', {
          text: 'cat .profile', errors: 60, finish: function () {
-            self.newAnswer('C/C++, PHP, HTML5, CSS3, JavaScript, Typescript, LESS, SCSS, Unit testing, SVN, SQLite, MySQL, Canvas, SWIFT\n' +
-               'jQuery, JSON, Apache Maven, Selenium Automated Test Tool, AngularJS, Gulp, NodeJS, Bootstrap (Framework), Ionic, Jasmine, Karma, Protractor\n' +
+            self.newAnswer('C/C++, PHP, HTML5, CSS3, JavaScript, Typescript, LESS, SCSS, Unit testing, SVN, SQLite, MySQL, Canvas, SWIFT, ' +
+               'jQuery, JSON, Apache Maven, Selenium Automated Test Tool, AngularJS, Gulp, NodeJS, Bootstrap (Framework), Ionic, Jasmine, Karma, Protractor, ' +
                'Git, Subversion, Ant, Slack, JIRA, Github, Webstorm, Apple Xcode, Jenkins, Crucible, Mac OS X, Asana, Sketch, Paintcode');
             self.lang();
          }
@@ -84,8 +84,25 @@ function Profile() {
       var self = this;
       Typer('#q_misc', {
          text: 'env', errors: 30, finish: function () {
-            self.newAnswer('Apple fanboy, comicnerd');
-           // self.misc();
+            self.newAnswer('father, married, apple fanboy, comicnerd');
+            self.contact();
+         }
+      });
+   };
+
+   /**
+    * contact
+    */
+   this.contact = function () {
+      this.newPrompt('q_contact');
+      var self = this;
+      Typer('#q_contact', {
+         text: 'more contact.txt', errors: 20, finish: function () {
+            self.newAnswer('<strong>mail</strong> <a href="mailto: info@danielkagemann.name">info@danielkagemann.name</a><br/>' +
+               '<strong>github</strong> = <a href="https://github.com/danielkagemann">https://github.com/danielkagemann</a><br/>' +
+               '<strong>codepen</strong> = <a href="https://codepen.io/danielkagemann">https://codepen.io/danielkagemann</a><br/>' +
+               '<strong>xing</strong> = <a href="https://www.xing.com/profile/Daniel_Kagemann">https://www.xing.com/profile/Daniel_Kagemann</a><br/>' +
+               '<strong>epilepsieTagebuch</strong> = <a href="http://hallo-ich-bin-epi.de/">http://hallo-ich-bin-epi.de/</a>');
          }
       });
    };
@@ -108,11 +125,14 @@ function Profile() {
     * exit routine
     */
    this.exit = function() {
+
+      // wait until all is ready
       this.newPrompt('askexit');
       var self = this;
       Typer('#askexit', {
          text: 'exit', errors: 40, finish: function () {
-            self.newAnswer('Tschüß &#xe41e;');
+            self.newAnswer('Tschüß');
+
          }
       });
    };
@@ -125,11 +145,11 @@ function Profile() {
 var prof = new Profile();
 
 document.querySelector('.close').addEventListener('click', function () {
-   prof.exit();
+   // prof.exit();
 });
 document.querySelector('.maximize').addEventListener('click', function () {
-   console.log('maximize');
+   // console.log('maximize');
 });
 document.querySelector('.minimize').addEventListener('click', function () {
-   console.log('minimize');
+   // console.log('minimize');
 });
